64be677dcddef1a9854ac55a0a8779812897b867
shivamkumarsingh28 <saeeamsingh@gmail.com>
2024-10-30 16:38:32 +0530

    final update

--- middleware.ts
+++ middleware.ts
@@ -4,17 +4,21 @@ import {jwtVerify, createRemoteJWKSet} from "jose";
 const hankoAPI = process.env.NEXT_PUBLIC_HANKO_API_URL || "";
 
 export async function middleware(req: NextRequest) {
-    const hanko = req.cookies.get("hanko")?.value;
-
-    const JWKS = createRemoteJWKSet(new URL(`${hankoAPI}/.well-known/jwks.json`))
-
-    try {
-        const verifiedJWT = await jwtVerify(hanko ?? "", JWKS );
-    } catch (error) {
-        console.log(error)
-        return NextResponse.redirect(new URL("/login", req.url));
+    const hanko = req.cookies.get("hanko")?.valueOf;
+  
+    if (!hanko) {
+      return NextResponse.redirect(new URL("/login", req.url));
     }
-}
+  
+    const JWKS = createRemoteJWKSet(new URL(`${hankoAPI}/.well-known/jwks.json`))
+  
+    try {
+      const verifiedJWT = await jwtVerify(hanko.toString(), JWKS);
+    } catch (error) {
+      console.log(error)
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
 
 export const config = {
     matcher: ["/user-page"]
--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",  // <=== enables static exports
-    distDir: "out",
-    reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
+const nextConfig = {}
+
+module.exports = nextConfig
